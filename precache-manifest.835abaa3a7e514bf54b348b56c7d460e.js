09a477be58a48d8ad1d6b1e153fb911fefcb0049
SrMeuNome <vinicius10001999@gmail.com>
2020-05-31 02:26:36 -0300

    Updates

---
+++ precache-manifest.835abaa3a7e514bf54b348b56c7d460e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d6124eaed640e7477783bcf6a9046c",
+    "url": "/srmeunome.github.io/GeraNicks/index.html"
+  },
+  {
+    "revision": "b5ccc4a39305e80de7bd",
+    "url": "/srmeunome.github.io/GeraNicks/static/css/main.a889148a.chunk.css"
+  },
+  {
+    "revision": "38c5f468e7537d31709a",
+    "url": "/srmeunome.github.io/GeraNicks/static/js/2.d6b51aa7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/srmeunome.github.io/GeraNicks/static/js/2.d6b51aa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5ccc4a39305e80de7bd",
+    "url": "/srmeunome.github.io/GeraNicks/static/js/main.2c02b36d.chunk.js"
+  },
+  {
+    "revision": "f066fcd93a5d39629e58",
+    "url": "/srmeunome.github.io/GeraNicks/static/js/runtime-main.bf4497b4.js"
+  },
+  {
+    "revision": "db06bb6080c639db691a3f9a28383f63",
+    "url": "/srmeunome.github.io/GeraNicks/static/media/FundoPretoEAzul.db06bb60.jpg"
+  }
+]);
