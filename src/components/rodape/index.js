14bf3b52d0282629030af520a26ab180d4d8e1de
Vinícius Gomes <vinigc19@gmail.com>
2020-08-27 20:22:06 -0300

    Organização Components & Alterações Visuais

--- src/components/rodape/index.js
+++ src/components/rodape/index.js
@@ -7,7 +7,7 @@ function Rodape(prop)
     <div className = 'bg-dark text-white pt-3 footer'>
       <h6>By: <a href="https://github.com/SrMeuNome">Sr Meu Nome</a></h6>
     </div>
-  )
+  );
 }
 
 export default Rodape;
